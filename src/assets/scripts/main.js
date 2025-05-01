9d162a0dceebdd40e842897e9d7e243c374ee7e8
Juan Ramon Vidal <jvidalpar@uoc.edu>
2025-05-01 19:05:49 +0200

    Quinto commit PEC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,17 +1,36 @@
 /**
  * Import dependencies from node_modules
- * see commented examples below
  */
-
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
+import "@fortawesome/fontawesome-free/css/all.min.css"; // <-- AFEGIT: Font Awesome
 
 /**
  * Write any other JavaScript below
  */
-
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
-import * as bootstrap from 'bootstrap';
+
+// AFEGIT: Detectar en quina pàgina estem i marcar el link actiu (versió millorada)
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll(".nav-link");
+
+  let currentPath = window.location.pathname;
+  currentPath = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+
+  // Normalitzem: si no hi ha res ("/") assumim "index.html"
+  if (currentPath === "" || currentPath === "/") {
+    currentPath = "index.html";
+  }
+
+  links.forEach(link => {
+    const linkHref = link.getAttribute("href");
+
+    if (linkHref === currentPath) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
